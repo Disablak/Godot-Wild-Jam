e1421536ec16967e280a1b690e6041817c4df223
disablak <disablaksteam@gmail.com>
2022-06-11 19:19:00 +0300

    up

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,14 +1,27 @@
 extends Node2D
 
 
+signal player_died
+
 var player
 var enemy
 
 
 func _ready():
-	player = find_node("PlayerKinematicBody")
+	player = find_node("Player")
 	enemy = find_node("Enemy")
 
 
 func _process(delta):
+	check_distance()
 	enemy.onTargetPositionChanged( player.position )
+	
+
+func check_distance():
+	var player_pos : Vector2 = player.position
+	var enemy_radius : int = enemy.radius_pxl
+	var distance = player_pos.distance_to(enemy.position) - enemy_radius
+	
+	if distance <= 0:
+		print("die")
+		emit_signal("player_died")
