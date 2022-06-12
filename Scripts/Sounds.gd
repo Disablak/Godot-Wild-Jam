c196a06c6f9a1d0607deed42f4003388a0cc4fa6
disablak <disablaksteam@gmail.com>
2022-06-12 20:40:08 +0300

    added sound money and finish

---
+++ Scripts/Sounds.gd
@@ -0,0 +1,22 @@
+extends AudioStreamPlayer
+
+
+export var clip_coin : AudioStreamSample
+export var clip_finish : AudioStreamSample
+
+
+func _ready():
+	SignalBus.connect(SignalBus.coin_took_name, self, "play_coin")
+	SignalBus.connect(SignalBus.level_comleted_name, self, "play_finish")
+
+
+func play_coin():
+	stream = clip_coin
+	play()
+	print("sound coin")
+
+
+func play_finish():
+	stream = clip_finish
+	play()
+	print("sound finish")
