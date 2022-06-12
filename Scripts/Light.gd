98daea666a8061f501aedb8f56d506c33a7b7898
disablak <disablaksteam@gmail.com>
2022-06-12 16:46:29 +0300

    spawn light

--- Scripts/Light.gd
+++ Scripts/Light.gd
@@ -1,6 +1,8 @@
+tool
 extends Light2D
 
 
+export var light_enabled = true setget set_light
 export(bool) var can_destroy = true
 var destroyed = false
 
@@ -13,3 +15,24 @@ func destroy_light():
 	destroyed = true
 	
 	$ExplosionParticle.play_particle()
+
+
+func init_light():
+	if not destroyed:
+		return
+	
+	enabled = true
+	destroyed = false
+
+
+func set_light(is_enable):
+	light_enabled = is_enable
+	enabled = is_enable
+	
+	property_list_changed_notify()
+
+
+func _on_Area2D_body_entered(body:Node):
+	if body.is_in_group("player"):
+		init_light()
+		print("light collision player")
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -31,3 +31,4 @@ func _physics_process(delta):
 
 func play_particle():
 	$ExplosionParticle.play_particle()
+
