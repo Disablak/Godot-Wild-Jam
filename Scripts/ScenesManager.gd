1003b863cf13348d01ec7998d1957c577ef8b2d4
Vlad <vladyslav.borodai@whaleapp.com>
2022-06-12 12:50:17 +0300

    no message

---
+++ Scripts/ScenesManager.gd
@@ -0,0 +1,33 @@
+extends Node
+
+
+export(Array, PackedScene) var scenesOrder : Array
+var currentLevelIndex: int = 0
+var activeScene 
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	SignalBus.connect(SignalBus.level_comleted_name, self, "moveToNextScene")
+
+func reloadScene():
+	moveToScene(currentLevelIndex)
+
+func moveToNextScene():
+	if scenesOrder.size() - 2 > currentLevelIndex:
+	currentLevelIndex += 1
+
+	moveToScene(currentLevelIndex)
+	pass
+
+func moveToScene(index : int):
+	if activeScene != null:
+		activeScene.queue_free()
+
+	activeScene = scenesOrder[index].instance()
+
+	add_child(activeScene)
+	pass
