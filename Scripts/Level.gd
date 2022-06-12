e27fd76e89387546b6b088e0ea7491ac4b11d02e
disablak <disablaksteam@gmail.com>
2022-06-12 12:11:01 +0300

    added keys

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -6,11 +6,14 @@ signal player_died
 var lights_can_destroy = []
 var player
 var enemy
+var need_keys_count
 
 
 func _ready():
 	$CanvasModulate.visible = true
+	$Background.visible = true
 	
+	find_all_keys()
 	find_all_destroyable_lights()
 	player = find_node("Player")
 	enemy = find_node("Enemy")
@@ -24,7 +27,18 @@ func _process(delta):
 
 
 func _on_Finish_body_entered(body:Node):
-	SignalBus.emit_signal(SignalBus.level_comleted_name)
+	if need_keys_count > 0:
+		print("find more keys ({0})".format([need_keys_count]))
+	else:
+		SignalBus.emit_signal(SignalBus.level_comleted_name)
+
+
+func _on_Key_took(key, body):
+	key.queue_free()
+	print("taken key!")
+	need_keys_count = need_keys_count - 1
+	if need_keys_count <= 0:
+		print("go to finish!")
 
 
 func find_all_destroyable_lights():
@@ -34,6 +48,15 @@ func find_all_destroyable_lights():
 			lights_can_destroy.append(light)
 	
 	print(lights_can_destroy)
+
+
+func find_all_keys():
+	var all_keys = get_tree().get_nodes_in_group("key")
+	for key in all_keys:
+		key.connect("key_took", self, "_on_Key_took")
+	
+	need_keys_count = all_keys.size()
+	print("need {0} keys!".format([need_keys_count]))
 
 
 func check_distance_enemy_and_player():
